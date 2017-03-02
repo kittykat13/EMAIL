849727f2fceda09a8b3c4c92f3f6c8e023952e69
Prince of Archives <rich.tijerina@gmail.com>
2017-03-02 16:09:36 -0700

    Update EM.pl

--- EM.pl
+++ EM.pl
@@ -5,7 +5,7 @@ use Email::Simple;
 use Try::Tiny;
 #################################
 # EM - automated group-based send
-#      password-protected pdf 
+#   password-protected-pdf attach 
 # SETUP #########################
 print "batch: ";
 my $batch = <>; chomp $batch;
@@ -18,10 +18,9 @@ print "Readn LIST\n";
 my %list; 
 # key = email_address; 
 # value = attachment;
-# attachment = PIN_filename; # ex: 4934_MNat;
 open(my $lifh, '<', 'LIST');
-my @pre = readline $lifh; chomp @pre;
-foreach $line (@pre) {
+my @set = readline $lifh; chomp @pre;
+foreach $line (@set) {
     my @i = split(" ", $line, 2);
     $list{$i[0]} = $i[1];
 }
@@ -33,19 +32,23 @@ my $trans = Email::Sender::Transport::SMTP::TLS->new(
     password => $pass,
 );
 # LOOP ############################
-my $email = Email::Simple->create(
-    header => [
-        To => $i,
-        From => $sender,
-        Subject => "Paystub $batch",
-    ],
-    body => "Pay Stub attached as a pdf\n",
-);
-try {
-    sendmail($email, { transport => $trans });
-} catch {
-    die "Error sending email: $_";
-};
+foreach $ee (keys %list) {
+    my $att = $list{$ee};
+    my $email = Email::Simple->create(
+        header => [
+            To => $ee,
+            From => $sender,
+            Subject => "Paystub $batch",
+            # ATTACH 
+        ],
+        body => "$batch pay stub attached as a pdf\n",
+    );
+    try {
+        sendmail($email, { transport => $trans });
+    } catch {
+        die "Error sending email: $_";
+    };
+}
 # NOTES ###########################
 # GOOGLE
 # If the sender is a google account change this setting
