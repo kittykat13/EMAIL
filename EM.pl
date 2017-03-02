83770b3c07df8c7406a7298c99409dca0ac20f2b
Prince of Archives <rich.tijerina@gmail.com>
2017-03-02 12:57:24 -0700

    Create EM.pl

---
+++ EM.pl
@@ -0,0 +1,43 @@
+use strict; use warnings;
+use Email::Sender::Simple qw(sendmail);
+use Email::Sender::Transport::SMTP::TLS qw();
+use Email::Simple;
+use Try::Tiny;
+#########################################
+# EM - automated send to group-recipients
+#                  see NOTE section below
+# SETUP #################################
+print "batch: ";
+my $batch = <>; chomp $batch;
+print "sender account: ";
+my $sender = <>; chomp $sender;
+print "$sender password: "; chomp $sender;
+my $pass = <>; chomp $pass;
+print "Readn 
+# SENDER ###############################
+my $trans = Email::Sender::Transport::SMTP::TLS->new(
+    host => 'smtp.gmail.com',
+    port => '587',
+    username => $sender,
+    password => $pass,
+);
+# LOOP #################################
+my $email = Email::Simple->create(
+    header => [
+        To => $i,
+        From => $sender,
+        Subject => "Paystub $batch",
+    ],
+    body => "Pay Stub attached as a pdf\n",
+);
+try {
+    sendmail($email, { transport => $trans });
+} catch {
+    die "Error sending email: $_";
+};
+
+# NOTES #################
+# GOOGLE
+# If the sender is a google account change this setting
+# https://www.google.com/settings/security/lesssecureapps
+
