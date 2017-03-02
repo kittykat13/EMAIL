f9e8692cdcfa94d9ddc6fba7441ba478637538b4
Prince of Archives <rich.tijerina@gmail.com>
2017-03-02 14:17:33 -0700

    Update EM.pl

--- EM.pl
+++ EM.pl
@@ -14,11 +14,18 @@ print "sender account: ";
 my $sender = <>; chomp $sender;
 print "$sender password: "; chomp $sender;
 my $pass = <>; chomp $pass;
+# LIST ##########################
 print "Readn LIST\n"; 
+my %list; 
+# key = email_address; 
+# value = attachment;
+# attachment = PIN_filename; # ex: 4934_MNat;
 open(my $lifh, '<', 'LIST');
-my @list = readline $lifh; chomp @list;
-
-
+my @pre = readline $lifh; chomp @pre;
+foreach $line (@pre) {
+    my @i = split(" ", $line, 2);
+    $list{$i[0]} = $i[1];
+}
 # SENDER #########################
 my $trans = Email::Sender::Transport::SMTP::TLS->new(
     host => 'smtp.gmail.com',
@@ -40,9 +47,7 @@ try {
 } catch {
     die "Error sending email: $_";
 };
-
 # NOTES ###########################
 # GOOGLE
 # If the sender is a google account change this setting
 # https://www.google.com/settings/security/lesssecureapps
-
