8c1b2167f4b2d1bc7497a44b1780fd091f1c5db8
Prince of Archives <rich.tijerina@gmail.com>
2017-03-14 14:06:04 -0600

    Update EM.pl

--- EM.pl
+++ EM.pl
@@ -1,13 +1,13 @@
 use strict; use warnings;
-use Email::Sender::Simple qw(sendmail);
+use Email::Stuffer qw(send);
+use Email::Sender::Simple; # qw(sendmail);
 use Email::Sender::Transport::SMTP::TLS qw();
-use Email::Simple;
-use Try::Tiny;
+#use Try::Tiny;
 #################################
 # EM - automated group-based send
 #   password-protected-pdf attach
 # DESC ##########################
-# prior steps: 
+# prior steps:
 # 1) PDF.vba - vba to pdf (if necessary)
 # 2) ATT.pl - password protect attachment
 # SETUP #########################
@@ -17,19 +17,15 @@ print "sender account: ";
 my $sender = <>; chomp $sender;
 print "$sender password: "; chomp $sender;
 my $pass = <>; chomp $pass;
+my %list; # key = email; value = attachment;
 # LIST ##########################
-print "Readn LIST\n"; 
-my %list; 
-# key = email_address; 
-# value = attachment;
-open(my $lifh, '<', 'LIST');
-my @set = readline $lifh; chomp @pre;
-foreach $line (@set) {
+print "Reading LIST.txt\n";
+open(my $lifh, '<', 'LIST.txt');
+my @set = readline $lifh; chomp @set;
+foreach my $line (@set) {
     my @i = split(" ", $line, 3);
     $list{$i[0]} = $i[1];
 }
-# SKIP rm key without file in ATT folder
-#    next unless exists $att;
 # SENDER #########################
 my $trans = Email::Sender::Transport::SMTP::TLS->new(
     host => 'smtp.gmail.com',
@@ -38,23 +34,18 @@ my $trans = Email::Sender::Transport::SMTP::TLS->new(
     password => $pass,
 );
 # LOOP ############################
-foreach $ee (keys %list) {
-    my $att = $list{$ee};
-
-    my $email = Email::Simple->create(
-        header => [
-            To => $ee,
-            From => $sender,
-            Subject => "Paystub $batch",
-            # ATTACH 
-        ],
-        body => "$batch pay stub attached as a pdf\n",
-    );
-    try {
-        sendmail($email, { transport => $trans });
-    } catch {
-        die "Error sending email: $_";
-    };
+foreach my $ee (keys %list) {
+    my $att = "ATT\\$list{$ee}";
+    print "sending $ee : $att\n";
+    my $body = "$batch pay stub attached as a pdf\n";
+    my $email = Email::Stuffer
+        ->from ($sender)
+        ->to ($ee)
+        ->subject($batch)
+        ->text_body($body)
+        ->attach_file($att)
+        ->transport($trans)
+        ->send or die "error in trans\n";
 }
 # NOTES ###########################
 # GOOGLE
