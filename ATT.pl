77bd237d3d0eb0bed3460082db69b42d842ae920
Prince of Archives <rich.tijerina@gmail.com>
2017-03-14 12:07:07 -0600

    Update ATT.pl

--- ATT.pl
+++ ATT.pl
@@ -5,12 +5,21 @@ use CAM::PDF;
 my $pre_location = 'PRE';
 my $final_location = 'ATT';
 # PRE/PIN_filename;
-# ex: PRE/4934_MNat.pdf;
-my @list = readdir($pre_location); chomp @list;
-foreach $i (@list) {
+# ex: PRE/4934_EEname.pdf;
+opendir(my $pre, $pre_location);
+#opendir(my $fin, $final_location);
+my @list = readdir($pre); chomp @list;
+shift @list; shift @list;
+print "@list\n";
+foreach my $i (@list) {
   my @tmp = split('_', $i, 2);
-  my $attach = $tmp[0]; my $pin = $tmp[1];
+  my $pin = $tmp[0]; my $attach = $tmp[1];
+  my $x = "$pre_location\\$i";
+  print "$x\n";
 # ENCRYPT WITH PIN #######
-  my $i = CAM::PDF->new();
+  my $pdf = CAM::PDF->new($x) or die "fail\n";
+  $pdf->setPrefs($pin,$pin);
 # SAVE FINAL LOC #########
+  my $doc = "$final_location\\$attach";
+  $pdf->output($doc);
 }
